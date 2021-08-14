17c9233a391985fab39bae5780d9f420409356e4
dmakeroam <cpfapp@gmail.com>
2021-08-14 23:16:10 +0700

    feat: prepare for github actions

--- Chapter06/src/main/java/com/packt/chapter6/reactive/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.packt.chapter6.reactive.repository;
-
-import com.packt.chapter6.reactive.model.Employee;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface EmployeeRepository extends ReactiveCrudRepository<Employee, Long> {
-}
--- Chapter06/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS employee;
-CREATE TABLE employee
-(
-    id         SERIAL PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name  VARCHAR(100) NOT NULL,
-    age        integer,
-    salary     decimal
-);
